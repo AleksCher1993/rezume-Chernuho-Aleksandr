5e610e81fcebde9257cb9886513d3c6be1f8f89e
Aleksandr_Chernuho <54019656+AleksCher1993@users.noreply.github.com>
2022-01-24 00:26:29 +0300

    Delete market directory

--- market/src/modules/getDate.js
+++
@@ -1,6 +0,0 @@
-const getDate = () => {
-  return fetch(
-    "https://mytestapp-253712-default-rtdb.firebaseio.com/goods.json"
-  ).then((response) => response.json());
-};
-export default getDate;
